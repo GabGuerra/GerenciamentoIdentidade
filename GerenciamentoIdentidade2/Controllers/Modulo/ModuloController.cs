15413a790fe20131772e47c80318923dfb293f8a
Gabriel Guerra <biel.guerra@hotmail.com>
2020-09-16 13:52:14 -0300

    #2 Início da tela de módulo

---
+++ GerenciamentoIdentidade2/Controllers/Modulo/ModuloController.cs
@@ -0,0 +1,16 @@
+﻿using GerenciamentoIdentidade2.Business;
+using GerenciamentoIdentidade2.Models.Login;
+using GerenciamentoIdentidade2.Models.Usuario;
+using System.Web.Mvc;
+
+namespace GerenciamentoIdentidade2.Controllers.Modulo
+{
+    public class ModuloController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("CadastroModuloIndex");
+        }     
+    }
+}
